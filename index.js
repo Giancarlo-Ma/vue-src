b3aad9011b345d5b5caf8adf833eb3d823886bde
guohangma <guohangma@gmail.com>
2021-07-09 17:22:43 +0800

    抽离watcher

--- index.js
+++ index.js
@@ -52,3 +52,47 @@ function remove(arr, item) {
 		}
 	}
 }
+
+// 属性变化通知window.target, 是一个watcher, 状态变化后通知watcher, 再通过watcher通知其他地方
+class Watcher {
+	constructor(vm, expOrFun, cb) {
+		this.vm = vm;
+		// 是一个函数,
+		this.getter = parsePath(expOrFun);
+		this.cb = cb;
+		this.value = this.get();
+	}
+
+	get() {
+		// 赋值给target,在reactive data的getter中可以添加这个依赖
+		window.target = this;
+		let value = this.getter.call(this.vm, this.vm);
+		// 避免单纯获取状态重复添加该watcher
+		window.target = undefined;
+		return value;
+	}
+
+	update() {
+		const oldValue = this.value;
+		// 再一次添加这个watcher
+		this.value = this.get();
+		this.cb.call(this.vm, this.value, oldValue);
+	}
+}
+
+const bailRE = /[^\w.$]/;
+function parsePath(path) {
+	if(!bailRE.test(path)) return;
+	const segments = path.split('.');
+	return function(obj) {
+		for(let i = 0; i < segments.length; i++) {
+			if(!obj) return;
+			obj = obj[segments[i]];
+		}
+		return obj;
+	}
+}
+
+// 实例化一个watcher,触发get方法->getter方法获取状态数据->触发reactive data的getter,向dep中添加依赖window.target
+// 当reactive data的状态变化,触发setter -> dep的notify -> watcher的update -> 回调被调用
+
